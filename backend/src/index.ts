4c9c3096534f19917661df2dc4e014e20a06c5d8
Fritiof Rusck <fritiof@rusck.se>
2024-02-15 12:05:09 +0100

    feat: add docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,10 @@
 require("module-alias/register");
 
-import express from "express";
-
 import { server, app } from "@/server";
 import getGameRoute from "@/routes/getGame";
 import makeMoveRoute from "@/routes/makeMove";
 
 const PORT = process.env["PORT"] ?? "3000";
-
-app.use(express.static("public"));
 
 getGameRoute(app);
 makeMoveRoute(app);
